740ddc39cbff6c066d60cd9ba675185e1715f1ed
Joey Robinson <theamazingness@gmail.com>
2019-02-12 01:37:47 -0600

    Playing around with a header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Button from '@material-ui/core/Button'
+import CustomizedDialogDemo from '../components/modal'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi</h1>
+    {/* <Button variant="contained" color="primary">
+      Hello World
+    </Button> */}
+    <CustomizedDialogDemo />
+    <br/>
   </Layout>
 )
 
